349bf4d837f31470f32e4255635ef1077018b25b
Stephanie <stephaniebtot3@gmail.com>
2025-02-28 10:57:16 -0700

    class update

--- Week 6/Student_api/index.js
+++ Week 6/Student_api/index.js
@@ -1,6 +1,8 @@
 const express = require('express'); //import express package
 const app = express();
 
+app.use(express.urlencoded({ extended: false })); //middleware
+
 //Student Data
 const students = [
     { id: 1, name: 'John Wick', department: 'Mathematics' },
@@ -10,7 +12,16 @@ const students = [
 
 //Get all students
 app.get('/students', function (req, res) {
-    res.status(200).send(students);
+
+    let student_data = [...students];
+//fi
+if (req.query.department) {
+    student_data = student_data.filter((student_item) => {
+        return student_item.department === req.query.department;
+    });
+}
+
+    res.status(200).send(student_data);
 });
 
 
@@ -30,14 +41,77 @@ app.get('/students/:student_id', function (req, res) {   //":student_id = param"
     }
 });
 
-
 //Create a new student
+app.post('/students', function (req, res) {
+    const student = req.body;
+    student.id = parseInt(student.id);
+    students.push(student); //adding a new item into the above students array
+    res.status(201).send(student);
+});
+
 
 //Update a student 
+app.put('/students/:student_id', function (req, res) {
+    const student_id = parseInt(req.params.student_id); //parseInt = convert string to number
+    //Get array index of student
+    const index = students.findIndex((student_item) => {
+        return student_item.id === student_id;
+    });
+
+    if (index >= 0) {
+        const student = req.body;
+        student.id = parseInt(student.id);
+
+        //update the student data
+        students[index] = student;
+
+        res.status(200).send(student);
+    } else {
+        res.status(404).send('unknown student');
+    }
+
+});
 
 //Update only department of a student
+app.patch('/students/:student_id', function (req, res) {
+    const student_id = parseInt(req.params.student_id); //parseInt = convert string to number
+    //Get array index of student
+    const index = students.findIndex((student_item) => {
+        return student_item.id === student_id;
+    });
+
+    if (index >= 0) {
+      const student = students[index]; //Get student data from students array
+
+        //update the student department
+        students[index].department = req.body.department;
+
+        res.status(200).send(student);
+    } else {
+        res.status(404).send('unknown student');
+    }
+
+});
 
 //Delete a student 
+app.delete('/students/:student_id', function (req, res) {
+    const student_id = parseInt(req.params.student_id); //parseInt = convert string to number
+    //Get array index of student
+    const index = students.findIndex((student_item) => {
+        return student_item.id === student_id;
+    });
+
+    if (index >= 0) {
+        const student = students[index];
+     //Delete student from student array
+     students.splice(index, 1);
+
+        res.status(200).send(student);
+    } else {
+        res.status(404).send('unknown student');
+    }
+
+});
 
 
 //Webserver
