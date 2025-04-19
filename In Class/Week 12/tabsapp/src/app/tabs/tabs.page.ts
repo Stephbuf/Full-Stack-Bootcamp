85cc5a3b6e7de7466620ad84852b0b6defbea2f8
Stephanie <stephaniebtot3@gmail.com>
2025-04-19 13:57:13 -0600

    week 11/12 update

---
+++ In Class/Week 12/tabsapp/src/app/tabs/tabs.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+  standalone: false,
+  
+})
+export class TabsPage {
+
+  constructor() {}
+
+}
